64cba8cc38c4f7bfc545cff33740657fec04ebe0
Sebas LG <neoranga55@gmail.com>
2016-09-22 00:38:10 +0200

    Kotlin classes 101

---
+++ app/src/main/java/com/sebaslogen/kotlinweatherapp/ForecastListAdapter.kt
@@ -0,0 +1,21 @@
+package com.sebaslogen.kotlinweatherapp
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+class ForecastListAdapter(val items: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}

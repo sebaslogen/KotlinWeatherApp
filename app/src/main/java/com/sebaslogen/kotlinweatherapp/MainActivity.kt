f896ab9a56f63ed9bf5542c8358d3a3eacdfabe3
Sebas LG <neoranga55@gmail.com>
2016-09-24 14:16:58 +0200

    Use Anko extension function for automatic casting

--- app/src/main/java/com/sebaslogen/kotlinweatherapp/MainActivity.kt
+++ app/src/main/java/com/sebaslogen/kotlinweatherapp/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.sebaslogen.kotlinweatherapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
     private val items = listOf(
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
     }

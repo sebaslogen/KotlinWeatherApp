cee1710ae1d005f1e5c3f39e78afd7be2a1ad011
Sebas LG <neoranga55@gmail.com>
2016-09-24 20:08:27 +0200

    Handle doAsync exceptions

--- app/src/main/java/com/sebaslogen/kotlinweatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/sebaslogen/kotlinweatherapp/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import com.sebaslogen.kotlinweatherapp.R
 import com.sebaslogen.kotlinweatherapp.domain.commands.RequestForecastCommand
 import org.jetbrains.anko.doAsync
@@ -15,13 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
-
         loadData(forecastList)
     }
 
     private fun loadData(forecastList: RecyclerView) {
         doAsync({
-
+            Log.e(javaClass.simpleName, "Error loading Forecast list")
         }, {
             val result = RequestForecastCommand("94043").execute()
             runOnUiThread {

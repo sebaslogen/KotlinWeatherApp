5c0555d72055e970d72abd5ff72db195fcf282c4
Sebas LG <neoranga55@gmail.com>
2016-11-09 23:22:32 +0100

    How to do RxJava observable on textChanged

--- app/src/main/java/com/sebaslogen/kotlinweatherapp/ui/activities/SettingsActivity.kt
+++ app/src/main/java/com/sebaslogen/kotlinweatherapp/ui/activities/SettingsActivity.kt
@@ -23,6 +23,24 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         cityCode.setText(zipCode.toString())
+        
+//        With RxJava it would use an Observable like:
+//
+//        Observable.create(Observable.OnSubscribe<String> { subscriber ->
+//            cityCode.addTextChangedListener(object : TextWatcher {
+//                override fun afterTextChanged(s: Editable?) = Unit
+//
+//                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+//
+//                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int)
+//                        = subscriber.onNext(s.toString())
+//            })
+//        }).debounce(1000, TimeUnit.MILLISECONDS)
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe({
+//                    text ->
+//                    zipCode = text.toLong()
+//                })
     }
 
     override fun onBackPressed() {

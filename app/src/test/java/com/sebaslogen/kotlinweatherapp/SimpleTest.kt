c8476e558c62c2e5caa2178fb6ec6560742fef33
Sebas LG <neoranga55@gmail.com>
2016-11-14 23:09:28 +0100

    Setup JUnit in Kotlin

---
+++ app/src/test/java/com/sebaslogen/kotlinweatherapp/SimpleTest.kt
@@ -0,0 +1,10 @@
+package com.sebaslogen.kotlinweatherapp
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SimpleTest {
+    @Test fun unitTestingWorks() {
+        assertTrue(true)
+    }
+}

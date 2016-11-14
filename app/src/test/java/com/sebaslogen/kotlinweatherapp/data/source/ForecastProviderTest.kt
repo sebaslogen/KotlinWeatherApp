d43896822a83b4a89cb3724ce57c141330d551e2
Sebas LG <neoranga55@gmail.com>
2016-11-15 00:10:31 +0100

    Add test with Mockito "when"

--- app/src/test/java/com/sebaslogen/kotlinweatherapp/data/source/ForecastProviderTest.kt
+++ app/src/test/java/com/sebaslogen/kotlinweatherapp/data/source/ForecastProviderTest.kt
@@ -1,8 +1,10 @@
 package com.sebaslogen.kotlinweatherapp.data.source
 
 import com.sebaslogen.kotlinweatherapp.domain.model.Forecast
+import com.sebaslogen.kotlinweatherapp.domain.model.ForecastList
 import org.junit.Assert.assertNotNull
 import org.junit.Test
+import org.mockito.Matchers.anyLong
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 
@@ -18,5 +20,19 @@ class ForecastProviderTest {
         val provider = ForecastProvider(listOf(dataSource))
         // Then
         assertNotNull(provider.requestForecast(0))
+    }
+
+    @Test fun emptyDatabaseReturnsServerValue() {
+        // Given
+        val db = mock(ForecastDataSource::class.java)
+        val server = mock(ForecastDataSource::class.java)
+        `when`(server.requestForecastByZipCode(anyLong(), anyLong()))
+                .then {
+                    ForecastList(0, "city", "country", listOf())
+                }
+        // When
+        val provider = ForecastProvider(listOf(db, server))
+        // Then
+        assertNotNull(provider.requestByZipCode(0, 0))
     }
 }

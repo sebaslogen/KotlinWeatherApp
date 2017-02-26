edbb710e5b25ace72faecd3eca5f30761a11ed5b
Sebas LG <neoranga55@gmail.com>
2017-02-26 17:12:05 +0100

    Extract DB store logic from Remote store logic

--- app/src/main/java/com/sebaslogen/kotlinweatherapp/data/remote/ForecastServer.kt
+++ app/src/main/java/com/sebaslogen/kotlinweatherapp/data/remote/ForecastServer.kt
@@ -8,7 +8,6 @@ class ForecastServer(val dataMapper: ServerDataMapper = ServerDataMapper()) : Fo
     override fun requestForecastByZipCode(zipCode: Long, date: Long): ForecastList? {
         val result = ForecastByZipCodeRequest(zipCode).execute()
         val converted = dataMapper.convertToDomain(zipCode, result) // TODO: This should be converted to a generic DataModel without knowledge of the domain layer
-//        forecastDb.saveForecast(converted)
         return converted
     }
 

a891e6f8d55e7bcd1b76a1aff1ce1ee17168a119
Sebas LG <neoranga55@gmail.com>
2017-04-30 00:43:41 +0200

    Migrate from Gson to Moshi for JSON deserialization

--- app/src/main/java/com/sebaslogen/kotlinweatherapp/data/remote/ForecastByZipCodeRequest.kt
+++ app/src/main/java/com/sebaslogen/kotlinweatherapp/data/remote/ForecastByZipCodeRequest.kt
@@ -1,10 +1,13 @@
 package com.sebaslogen.kotlinweatherapp.data.remote
 
-import com.google.gson.Gson
 import com.sebaslogen.kotlinweatherapp.data.remote.model.ForecastResult
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
 import java.net.URL
 
-class ForecastByZipCodeRequest(val zipCode: Long, val gson: Gson = Gson()) {
+class ForecastByZipCodeRequest(val zipCode: Long,
+                               val jsonAdapter: JsonAdapter<ForecastResult> = Moshi.Builder()
+                                       .build().adapter(ForecastResult::class.java)) {
 
     companion object {
         private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
@@ -14,6 +17,6 @@ class ForecastByZipCodeRequest(val zipCode: Long, val gson: Gson = Gson()) {
 
     fun execute(): ForecastResult {
         val forecastJsonStr = URL(COMPLETE_URL + zipCode).readText()
-        return gson.fromJson(forecastJsonStr, ForecastResult::class.java)
+        return jsonAdapter.fromJson(forecastJsonStr)
     }
 }
